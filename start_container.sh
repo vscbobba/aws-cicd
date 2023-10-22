66fad8c12052f91a48a3e373e283f0ac2f29084f
vscbobba <vscbobba@gmail.com>
2023-10-22 14:38:49 +0530

    updated start and stop script

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull chowdarybvsn/sample-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 chowdarybvsn/sample-python-app
